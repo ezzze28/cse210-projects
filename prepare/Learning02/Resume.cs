4f457994b749f15ded840287eeee7e645193ada3
ezzze28 <papitomiret@gmail.com>
2023-01-28 01:42:54 -0300

    resume updt

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -3,6 +3,8 @@ using System;
 public class Resume
 {
     public string _name;
+
+    // Make sure to initialize your list to a new List before you use it.
     public List<Job> _jobs = new List<Job>();
 
     public void Display()
@@ -10,9 +12,10 @@ public class Resume
         Console.WriteLine($"Name: {_name}");
         Console.WriteLine("Jobs:");
 
-
+        // Notice the use of the custom data type "Job" in this loop
         foreach (Job job in _jobs)
         {
+            // This calls the Display method on each job
             job.Display();
         }
     }
